895e6ad5707033483088e5a944a3e67b5b25330c
joker--079 <166355797+joker079@users.noreply.github.com>
2024-05-16 19:27:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ function convertToHex() {
     // 結果を表示する要素を取得
     const resultElement = document.getElementById('hexResult');
 
-    // UTF-16の16進数への変換処理
+    // Shift JISの16進数への変換処理
+    let sjisArray = Encoding.convert(Encoding.stringToCode(inputText), 'SJIS');
     let hexResult = '';
-    for (let i = 0; i < inputText.length; i++) {
-        // 各文字をUTF-16の16進数に変換
-        let hex = inputText.charCodeAt(i).toString(16).toUpperCase();
-        // 必要に応じて4桁にパディング
-        hex = ('0000' + hex).slice(-4);
+    for (let i = 0; i < sjisArray.length; i++) {
+        // 各バイトを16進数に変換
+        let hex = sjisArray[i].toString(16).toUpperCase();
+        // 必要に応じて2桁にパディング
+        hex = ('00' + hex).slice(-2);
         hexResult += hex;
     }
 
